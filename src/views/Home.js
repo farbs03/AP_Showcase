09d8f4e3f018272c00abc57dd6841ebc31314cb1
farbs03 <cgeraldfarber@icloud.com>
2021-01-19 15:25:37 -0500

    changed title

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Card, Divider, Row, Col, Popover} from 'antd';
+import { Card, Divider, Row, Col, Popover, Dropdown, Menu} from 'antd';
 import Navbar from '../util/Navbar'
-import {MailOutlined, LinkOutlined, GithubOutlined, InstagramOutlined} from '@ant-design/icons'
+import {MailOutlined, LinkOutlined, GithubOutlined, InstagramOutlined, DownOutlined} from '@ant-design/icons'
 
 import '../util/Navbar.js'
 
@@ -17,6 +17,7 @@ for(var i = 0; i < length1; i++) {
 for(var i = 0; i < length2; i++) {
     sec2[i].Email = "mailto:" + sec2[i].Email;
 }
+
 const HSEApps = (
     <div style={{fontSize: "14px"}, {paddingBottom: "0px"}}>
         <p style={{textAlign: 'center'}, {display: 'inline-block'}}>Made by HSE Apps</p>
@@ -24,6 +25,27 @@ const HSEApps = (
         <a href='https://github.com/HSE-Apps' target='_blank' class="social"><GithubOutlined/></a>
     </div>
 )
+
+const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <a href="https://www.loom.com/share/d53eff425e654a0592f462588a6e6586" target='_blank'>Student Media</a>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <a href="https://www.loom.com/share/909aa17f53284a6fa988ebadbc240853" target='_blank'>Digital Media</a>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <a href="https://www.youtube.com/watch?v=rfy3A5tRM-8" target='_blank'>Animal Science</a>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <a href="https://youtube.com/watch?v=LMJF5fqE3pY" target='_blank'>Advanced Life Science: Animals</a>
+      </Menu.Item>
+      <Menu.Item key="4">
+        <a href="https://www.youtube.com/watch?v=05wwOtZV6Y0" target='_blank'>Agriculture Power, Structure and Technology</a>
+      </Menu.Item>
+    </Menu>
+);
+
 class Home extends React.Component {
     render () {
         return (
@@ -33,14 +55,20 @@ class Home extends React.Component {
 
                     <div style={{}}>
                         <h2 class="ant-typography">HSE AP / Dual Credit & Academic Specialty Showcase - 1/21/21</h2>
-                        <span class="ant-typography ant-typography-secondary" style={{fontSize: '18px'}}>Join live Zoom Q&A sessions below</span>
+                        <div style={{fontSize: "14px"}, {paddingBottom: "0px"}}>
+                            <p style={{textAlign: 'center'}, {display: 'inline-block'}}>Made by HSE Apps</p>
+                            <a href='https://www.instagram.com/hseapps/' target='_blank' class="social"><InstagramOutlined/></a>
+                            <a href='https://github.com/HSE-Apps' target='_blank' class="social"><GithubOutlined/></a>
+                        </div>
+                        <span class="ant-typography ant-typography-secondary" style={{fontSize: '20px'}}>Join live Zoom Q&A sessions below or</span>
+                        <br></br>
+                        <Dropdown overlay={menu} trigger={['click']}>
+                            <a className="ant-dropdown-link" onClick={e => e.preventDefault()} style={{fontSize: "16px"}}>
+                                View pre-recorded/archived videos<DownOutlined />
+                            </a>
+                        </Dropdown>
                     </div>
                     <br></br>
-                    <div style={{fontSize: "14px"}, {paddingBottom: "0px"}}>
-                        <p style={{textAlign: 'center'}, {display: 'inline-block'}}>Made by HSE Apps</p>
-                        <a href='https://www.instagram.com/hseapps/' target='_blank' class="social"><InstagramOutlined/></a>
-                        <a href='https://github.com/HSE-Apps' target='_blank' class="social"><GithubOutlined/></a>
-                    </div>
                     <div class="slots">
 
                         <div class='time-slot' id="section1" style={{display: 'block'}}>
